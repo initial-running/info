56ed58ed6805c810ea6192701981a6e59836c6fd
Lucas Carvalho <lucashms.carvalho@gmail.com>
2022-09-24 17:27:21 -0300

    atualiza a seção autores

--- js/app.js
+++ js/app.js
@@ -4,3 +4,17 @@ $(function () {
     $('div#tipo1').addClass('collapse');
   }
 });
+
+$(function () {
+  const today = new Date();
+  const ageMaria = today.getFullYear() - 2002;
+  const ageCainan = today.getFullYear() - 2002;
+
+  if (!document.querySelector('#age-maria')) return;
+  
+  const ageMariaElement = document.querySelector('#age-maria');
+  const ageCainanElement = document.querySelector('#age-cainan');
+
+  ageMariaElement.innerHTML = ageMaria;
+  ageCainanElement.innerHTML = ageCainan;
+});
